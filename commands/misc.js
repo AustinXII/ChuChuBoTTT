81b08bc135cc320d0b3398fe00740b2668696bdc
Austin Couturier <pingas311@hotmail.ca>
2015-10-21 16:32:12 -0600

    Update misc.js

--- commands/misc.js
+++ commands/misc.js
@@ -534,7 +534,7 @@ exports.commands = {
 		delete timer.on[room];
 	},
 	atomic: function(arg, by, room) {
-		Bot.say(by, room, 'You can see the rules of atomic chess' + '<a href=\"https://en.wikipedia.org/wiki/Atomic_chess\">Here</a><br />');
+		Bot.say(by, room, 'You can see the rules of atomic chess here: https://en.wikipedia.org/wiki/Atomic_chess');
 	}
 };
 
